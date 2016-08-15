3ee84122cdb2cc3d1669956eb5d4fe2405bf2d73
Craig Dennis <craig@teamtreehouse.com>
2016-08-15 16:40:46 -0700

    Adds Java 8 flavored observer pattern

--- gold-watcher/src/main/java/com/teamtreehouse/goldwatcher/Example.java
+++ gold-watcher/src/main/java/com/teamtreehouse/goldwatcher/Example.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.goldwatcher;
 
 import com.teamtreehouse.goldwatcher.tools.LedLight;
 import com.teamtreehouse.goldwatcher.tools.MegaTron;
+import com.teamtreehouse.goldwatcher.tools.Tweeter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +16,12 @@ public class Example {
         MegaTron megaTron = new MegaTron();
         LedLight light = new LedLight();
         Map<String, String[]> colorsByCountry = getColorsByCountry();
+
+        reporter.addObserver("tweet",
+                country -> Tweeter.tweet(country + " just won the Gold!  #olympics"));
+        reporter.addObserver("megatron", megaTron::display);
+        reporter.addObserver("lights", country -> light.flashColors(colorsByCountry.get(country)));
+
         reporter.reportWin("USA");
     }
 
--- gold-watcher/src/main/java/com/teamtreehouse/goldwatcher/GoldMedalReporter.java
+++ gold-watcher/src/main/java/com/teamtreehouse/goldwatcher/GoldMedalReporter.java
@@ -1,9 +1,29 @@
 package com.teamtreehouse.goldwatcher;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class GoldMedalReporter {
+    private Map<String, Consumer<String>> observers = new HashMap<>();
+
+
+    public void addObserver(String key, Consumer<String> observer) {
+        observers.put(key, observer);
+    }
+
+    public void removeObserver(String key) {
+        observers.remove(key);
+    }
+
+    public void notifyObservers(String country) {
+        observers.values().forEach(fn -> fn.accept(country));
+    }
+
 
     public void reportWin(String country) {
         System.out.println("Gold medal awarded to " + country);
+        notifyObservers(country);
     }
 
 
